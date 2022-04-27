23ca4b9a382a12b50800b231c60723c658697733
Fawazahmed12 <fawaz@provility.com>
2022-04-27 18:09:46 +0530

    day-3 exercises working in progress

--- day-2-exercises/sum.js
+++ day-2-exercises/sum.js
@@ -8,27 +8,88 @@
 // console.log(result)
 
 //sum of arrays:
-const numbers = [1,2,3]
- function sumOfNumbers(sum){
-   let num = 0;
-    for(let i=0;i<sum.length;i++){
+// const numbers = [1,2,3]
+//  function sumOfNumbers(sum){
+//    let num = 0;
+//     for(let i=0;i<sum.length;i++){
        
-       num = num + sum[i]
-    }
-    return num;
- }
+//        num = num + sum[i]
+//     }
+//     return num;
+//  }
 
- let result1 = sumOfNumbers(numbers)
- console.log(result1)
+//  let result1 = sumOfNumbers(numbers)
+//  console.log(result1)
 
- 
+
  ////3
-const input = [fawaz,fawad,ramaesh,faiyaz,ravi]
+// const input = [fawaz,fawad,ramaesh,faiyaz,ravi]
 
-function occurrences(){
+// function occurrences(){
 
+// }
+
+// let result = occurrences(input)
+// console.log(result)
+ 
+
+//4
+// const numbers = [1,4,8,10]
+
+// function findMax(input) {
+//    let largestNum = input[0];
+//    for (let i = 0; i < input.length; i++) {
+//      if (input[i] > largestNum) {
+//        largestNum = input[i];
+//      }
+//    }
+//    return largestNum;
+//  }
+
+
+//  console.log(findMax(numbers))
+
+
+ //5 find the largest number in two numbers
+//  function findMax(num1,num2){
+//     if(num1>num2){
+//       return num1;
+//     }else{
+//       return num2
+//     }
+//  }
+
+//  console.log(findMax(8,3))
+
+
+ //6
+
+
+//  function findGreatestNumber(numbers){
+//   let maxNum = 0;
+//   for(let index=0; index<numbers.length; index++){
+
+//     const value = numbers[index]
+//     if(maxNum<value){
+//       maxNum =value;
+//     }
+
+//   }
+//   return maxNum
+// }
+
+// const input = [2,10,3]
+// const result = findGreatestNumber(input)
+// console.log(result)
+
+
+//7.
+
+const a = [1,2,3,4,5,6];
+const lastvalue = a[a.length-1];
+for(let i=a.length-1;i>=0;i--){
+  a[i] = a[i-1];
 }
 
-let result = occurrences(input)
-console.log(result)
- 
+a[0] = lastvalue;
+console.log(a)
---
+++ day-3-exercises/level-1.js
@@ -0,0 +1,78 @@
+//1.Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+
+
+const firstName = 'john'
+const lastName = 'pollard'
+const country = 'india'
+const city = 'chennai'
+const age = 23
+const isMarried = 'single'
+const year = 2023
+
+
+console.log(typeof(firstName))
+console.log(typeof(lastName))
+console.log(typeof(country))
+console.log(typeof(city))
+console.log(typeof(age))
+console.log(typeof(isMarried))
+console.log(typeof(year))
+
+//2.Check if type of '10' is equal to 10
+
+console.log(10 == '10')           // true, compare only value
+console.log(10 === '10')          // false, compare both value and data type
+
+//3.Check if parseInt('9.8') is equal to 10
+
+let number = '9.8'
+let number2 = parseInt(number)
+let number1 = 10
+
+console.log(number2 == number1) 
+
+//4.Boolean value is either true or false.
+
+//Write three JavaScript statement which provide truthy value.
+
+let isLightOn = true
+let num = 2
+let name = 'fawaz'
+
+//Write three JavaScript statement which provide falsy value.
+
+// let firstName
+// console.log(firstName
+
+let empty = null
+console.log(empty) 
+
+let number3  = 0
+console.log(number3)
+
+
+//5.Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+
+4 > 3 //true
+console.log(4>3)
+4 >= 3 // true
+console.log(4>=3)
+4 < 3 //false
+console.log(4<3)
+4 <= 3 //false
+console.log(4<=3)
+4 == 4 // true
+console.log(4==4)
+4 === 4 //true
+console.log(4===4)
+4 != 4 //false
+console.log(4!=4)
+4 !== 4 //true
+console.log(4!==4)
+4 != '4' //false
+console.log(4!='4')
+4 == '4' //false
+console.log(4 =='4')
+4 === '4' //false
+console.log(4 ==='4')
+// Find the length of python and jargon and make a falsy comparison statement.
