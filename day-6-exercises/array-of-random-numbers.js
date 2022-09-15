d651b6527945496b49eba65f8e1113829bc5b959
fawaz <fawaz@provility.com>
2022-09-15 18:14:01 +0530

    exercise

--- day-6-exercises/array-of-random-numbers.js
+++ day-6-exercises/array-of-random-numbers.js
@@ -1,3 +1,20 @@
 // 13
 
 // Develop a small script which generate array of 5 random numbers
+
+for (let i = 0; i < 5; i++) {
+    console.log(Math.floor(Math.random() * 100))
+}
+   
+
+//Develop a small script which generate array of 5 random numbers and the numbers must be unique
+
+
+// 15
+//Develop a small script which generate a six characters random id:
+
+for (let i = 0; i < 1; i++) {
+    
+    console.log(
+        Math.random().toString(36).replace(/[^a-z]+/g, ''))
+}
--- day-6-exercises/prime-numbers.js
+++ day-6-exercises/prime-numbers.js
@@ -20,7 +20,7 @@
 
 function isPrime(array) {
     for(var i = 0; i <= array.length; i++){
-    
+ 
       if(array[i] % 2 === 0) {
  console.log("prime ")
        }
